0cf36ce509f182f3a3c2c17e9a9429ee8ed716ae
Daniel Lupacescu <39854697+DanL1L@users.noreply.github.com>
2025-07-07 13:52:03 +0300

    Update and rename streamlit_app.py to app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Define the pages
+main_page = st.Page("pages/Indicatori_Macro.py", title="Indicatorii MacroEconomici")
+page_2 = st.Page("pages/Prognoza.py", title="Prognoza")
+page_3 = st.Page("pages/Date.py", title="Date")
+
+# Set up navigation
+pg = st.navigation([main_page, page_2, page_3])
+
+# Run the selected page
+pg.run()
