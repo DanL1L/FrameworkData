872d5cb3ad58d73e5c1a1e0d382eec5312cccb9c
Daniel Lupacescu <39854697+DanL1L@users.noreply.github.com>
2025-07-08 13:52:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
 # Define the pages
-main_page = st.Page("Indicatori_Macro.py", title="Indicatorii MacroEconomici")
+main_page = st.Page("pages/Indicatori_Macro.py", title="Indicatorii MacroEconomici")
 page_2 = st.Page("pages/Prognoza.py", title="Prognoza")
 page_3 = st.Page("pages/Export.py", title="Export")
 page_4 = st.Page("pages/Import.py", title="Import")
-page_5 = st.Page("pages/Raport_PDF.py", title="Rapoarte")
 
 # Set up navigation
-pg = st.navigation([main_page, page_2, page_3, page_4, page_5])
+pg = st.navigation([main_page, page_2, page_3, page_4])
 
 # Run the selected page
 pg.run()
