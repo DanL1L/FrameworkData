f13859dd098611d76f3253ffae4ef641c2f82edf
Daniel Lupacescu <39854697+DanL1L@users.noreply.github.com>
2025-11-05 16:40:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,16 @@
 import streamlit as st
 
 # Define the pages
-main_page = st.Page("Main.py", title="Indicatorii MacroEconomici")
-page_2 = st.Page("Real.py", title="Sectorul Real")
-page_3 = st.Page("Indicatori_Macro.py", title="Sectorul Extern")   
-page_4 = st.Page("Monetar.py", title="Sectorul Monetar")
-page_5 = st.Page("Public.py", title="Sectorul Public")
-page_6 = st.Page("Social.py", title="Sectorul Social")
+main_page = st.Page("pages/Main.py", title="Indicatorii MacroEconomici")
+page_2 = st.Page("pages/Real.py", title="Sectorul Real")
+page_3 = st.Page("pages/Indicatori_Macro.py", title="Sectorul Extern")   
+page_4 = st.Page("pages/Monetar.py", title="Sectorul Monetar")
+page_5 = st.Page("pages/Public.py", title="Sectorul Public")
+page_6 = st.Page("pages/Social.py", title="Sectorul Social")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3, page_4, page_5, page_6])
 
 # Run the selected page
+
 pg.run()
