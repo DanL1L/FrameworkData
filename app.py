ccfdb07b2d14cdbd2f85dfce15144d7a9296d2fd
Daniel Lupacescu <39854697+DanL1L@users.noreply.github.com>
2025-11-05 16:37:26 +0200

    Fix page references in Streamlit app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # Define the pages
-main_page = st.Page("Main.py", title="Indicatorii MacroEconomici")
+main_page = st.Page("Real.py", title="Indicatorii MacroEconomici")
 page_2 = st.Page("Real.py", title="Sectorul Real")
 page_3 = st.Page("Indicatori_Macro.py", title="Sectorul Extern")   
 page_4 = st.Page("Monetar.py", title="Sectorul Monetar")
@@ -13,4 +13,5 @@ page_6 = st.Page("Social.py", title="Sectorul Social")
 pg = st.navigation([main_page, page_2, page_3, page_4, page_5, page_6])
 
 # Run the selected page
+
 pg.run()
