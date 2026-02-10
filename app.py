ec94fc6c3eda8db0ee56ad278d283b7152996db5
Daniel Lupacescu <39854697+DanL1L@users.noreply.github.com>
2026-02-10 10:06:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ page_2 = st.Page("pages/Real.py", title="Sectorul Real")
 page_3 = st.Page("pages/Indicatori_Macro.py", title="Sectorul Extern")   
 page_4 = st.Page("pages/Monetar.py", title="Sectorul Monetar")
 page_5 = st.Page("pages/Public.py", title="Sectorul Public")
-page_6 = st.Page("pages/Social.py", title="Sectorul Social")
+page_6 = st.Page("pages/Link_BNS.py", title="Sectorul Social")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3, page_4, page_5, page_6])
@@ -15,4 +15,5 @@ pg = st.navigation([main_page, page_2, page_3, page_4, page_5, page_6])
 # Run the selected page
 
 pg.run()
+
 
